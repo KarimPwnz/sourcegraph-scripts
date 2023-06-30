062764949bc00aec763d65136a2a58376b4158e9
Karim Rahal <karim@karimrahal.com>
2023-06-29 18:49:07 -0700

    Add proxy flag

--- scripts/github_downloader.py
+++ scripts/github_downloader.py
@@ -6,21 +6,21 @@ import aiohttp
 
 from aiopath import AsyncPath
 
-
 class NotFoundException(Exception):
     pass
 
 class GitHubDownloader:
-    def __init__(self, client, output_path):
+    def __init__(self, client, output_path, proxy):
         self._client = client
         self.output_path = output_path
+        self.proxy = proxy
 
     @staticmethod
     def _url_to_path(url):
         return url.replace("https://raw.githubusercontent.com/", "")
 
     async def download(self, url):
-        async with self._client.get(url) as resp:
+        async with self._client.get(url, proxy=self.proxy) as resp:
             resp.raise_for_status()
             return await resp.text()
 
@@ -58,15 +58,22 @@ async def main():
     )
     parser.add_argument(
         "-l",
+        "--limit",
         dest="limit",
         help="Concurrent requests limit (default: 100)",
         default=100,
         type=int,
     )
+    parser.add_argument("-p",
+        "--proxy",
+        dest="proxy",
+        help="Proxy to use with each request; proxy domain resolved per request",
+        default=None
+    )
     args = parser.parse_args()
     conn = aiohttp.TCPConnector(limit=args.limit)
     async with aiohttp.ClientSession(connector=conn) as client:
-        downloader = GitHubDownloader(client=client, output_path=args.output)
+        downloader = GitHubDownloader(client=client, output_path=args.output, proxy=args.proxy)
         tasks = []
         urls = (l.rstrip("\n") for l in sys.stdin)
         for url in urls:
